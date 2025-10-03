eacaed277a557e01d9821134428b3757d00d0c2e
VeronikaVS90 <vsokolovanika@gmail.com>
2025-10-03 11:37:13 +0300

    add navigation to the board list page

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,21 @@
 import { AppBar, Toolbar, IconButton } from "@mui/material";
 import Brightness4Icon from "@mui/icons-material/Brightness4";
 import { useStore } from "../store/useStore";
+import { useNavigate } from "react-router-dom";
 
 const Header = () => {
   const store = useStore();
+  const navigate = useNavigate();
 
   return (
     <AppBar position="static">
       <Toolbar>
-        <div style={{ flex: 1 }}>My To Do App</div>
+        <div
+          style={{ flex: 1, cursor: "pointer" }}
+          onClick={() => navigate("/")}
+        >
+          My To Do App
+        </div>
         <IconButton color="inherit" onClick={() => store.ui.toggleTheme()}>
           <Brightness4Icon />
         </IconButton>
